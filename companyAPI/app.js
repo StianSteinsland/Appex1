7aade89d0215140006e7add525d98eb8723d76e4
StianSteinsland <stian.21@live.no>
2023-05-25 00:44:47 +0200

    Endret videre på nettsiden, API

--- companyAPI/app.js
+++ companyAPI/app.js
@@ -2,12 +2,29 @@ document.getElementById('company-form').addEventListener('submit', async functio
     event.preventDefault();
 
     const orgnr = document.getElementById('orgnr').value;
+    const note = document.getElementById('note').value;
 
     try {
-        const response = await fetch(`http://localhost:3000/company/${orgnr}`);
-        const data = await response.json();
-        const companyInfoDiv = document.getElementById('company-info');
-        companyInfoDiv.textContent = `Company name: ${data.name}`;
+        // Send a POST request to save the company info and the note
+        const response = await fetch('http://localhost:3000/company', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ orgnr, note })
+        });
+
+        // Check if the request was successful
+        if (response.ok) {
+            const data = await response.json();
+
+            // Fetch and display the company info
+            const companyInfo = await fetch(`http://localhost:3000/company/${orgnr}`).then(res => res.json());
+            const companyInfoDiv = document.getElementById('company-info');
+            companyInfoDiv.textContent = `Company name: ${companyInfo.name}, Note: ${note}`;
+        } else {
+            console.log('Error saving company info: ', await response.text());
+        }
     } catch(err) {
         console.log('Fetch error: ', err);
     }
