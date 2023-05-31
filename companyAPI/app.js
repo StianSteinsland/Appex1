3bbb9a8c3313cc8af876d144d33ac55ff07e97b5
StianSteinsland <stian.21@live.no>
2023-05-31 15:13:58 +0200

    Lagt til flere informasjonsvalg.

--- companyAPI/app.js
+++ companyAPI/app.js
@@ -1,71 +1,84 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-
-    document.getElementById('submit').addEventListener('click', async function(event) {
-        event.preventDefault();
-        
-        let orgnr = document.getElementById('orgnr').value;
-
-        // Sjekk om orgnr er definert
-        if (!orgnr) {
-            console.error("Orgnr must be provided.");
-            return;
-        }
-
-        try {
-        // Hent og vis informasjon om bedriften
+    document.getElementById('submit').addEventListener('click', async function (event) {
+      event.preventDefault();
+  
+      let orgnr = document.getElementById('orgnr').value;
+  
+      // Check if orgnr is provided
+      if (!orgnr) {
+        console.error('Orgnr must be provided.');
+        return;
+      }
+  
+      try {
+        // Fetch and display company information
         const response = await fetch(`http://localhost:3000/company/${orgnr}`);
         console.log('HTTP Response:', response);
         const companyInfo = await response.json();
-        
-
-        // Logg hele responsen til konsollen
+  
+        // Log the entire response to the console
         console.log('API Response:', companyInfo);
-
+  
         let navn = companyInfo.navn || '';
-        let organisasjonsform = companyInfo.organisasjonsform ? companyInfo.organisasjonsform.beskrivelse : '';
-        let adresse = companyInfo.forretningsadresse ? companyInfo.forretningsadresse.adresse.join(', ') : '';
-
-            const companyInfoDiv = document.getElementById('company-info');
-            companyInfoDiv.textContent = `Company name: ${navn}, Orgnr: ${orgnr}, Organisasjonsform: ${organisasjonsform}, Adresse: ${adresse}`;
-        } catch(err) {
-            console.log('Fetch error: ', err);
-        }
+        let organisasjonsform = companyInfo.organisasjonsform || '';
+        let adresse = companyInfo.adresse || '';
+        let hjemmeside = companyInfo.additionalData.hjemmeside || '';
+        let naeringskode1 = companyInfo.additionalData.naeringskode1 || '';
+        let antallAnsatte = companyInfo.additionalData.antallAnsatte || '';
+        let postnummer = companyInfo.additionalData.postnummer || '';
+        let poststed = companyInfo.additionalData.poststed || '';
+  
+        const companyInfoDiv = document.getElementById('company-info');
+        companyInfoDiv.innerHTML = `
+          <p>Bedrift navn: ${navn}</p>
+          <p>Orgnr: ${orgnr}</p>
+          <p>Organisasjonsform: ${organisasjonsform}</p>
+          <p>Adresse: ${adresse}</p>
+          <p>Hjemmeside: ${hjemmeside}</p>
+          <p>Naeringskode: ${naeringskode1}</p>
+          <p>Antall Ansatte: ${antallAnsatte}</p>
+          <p>Postnummer: ${postnummer}</p>
+          <p>Poststed: ${poststed}</p>
+        `;
+      } catch (err) {
+        console.log('Fetch error: ', err);
+      }
     });
-
-    document.getElementById('save-info').addEventListener('click', async function(event) {
-        event.preventDefault();
-
-        const orgnr = document.getElementById('orgnr').value;
-        const additionalInfo = document.getElementById('additional-info').value;
-
-        // Sjekk om orgnr og additionalInfo er definert
-        if (!orgnr || !additionalInfo) {
-            console.error("Orgnr and additional info must be provided.");
-            return;
+  
+    document.getElementById('save-info').addEventListener('click', async function (event) {
+      event.preventDefault();
+  
+      const orgnr = document.getElementById('orgnr').value;
+      const additionalInfo = document.getElementById('additional-info').value;
+  
+      // Check if orgnr and additionalInfo are provided
+      if (!orgnr || !additionalInfo) {
+        console.error('Orgnr and additional info must be provided.');
+        return;
+      }
+  
+      try {
+        // Send a POST request to save the information and note about the company
+        const response = await fetch('http://localhost:3000/company', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ orgnr, additionalData: { note: additionalInfo } }),
+        });
+        const data = await response.json();
+        console.log('Additional info saved:', data);
+        alert('Info saved!');
+  
+        // Check if the request was successful
+        if (response.ok) {
+          console.log('Company info saved successfully');
+        } else {
+          console.log('Error saving company info: ', await response.text());
         }
-
-        try {
-            // Send en POST forespørsel for å lagre informasjonen og notatet om bedriften
-            const response = await fetch('http://localhost:3000/company', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ orgnr, additionalInfo })
-            });
-            const data = await response.json();
-            console.log('Additional info saved:', data);
-            alert('Info saved!');
-        
-            // Sjekk om forespørselen var suksessfull
-            if (response.ok) {
-                console.log('Company info saved successfully');
-            } else {
-                console.log('Error saving company info: ', await response.text());
-            }
-        } catch(err) {
-            console.log('Fetch error: ', err);
-        }
+      } catch (err) {
+        console.log('Fetch error: ', err);
+      }
     });
-});
-
+  });
+  
--- companyAPI/index.js
+++ companyAPI/index.js
@@ -13,45 +13,62 @@ let companies = [];
 
 app.get('/company/:orgnr', async (req, res) => {
     const orgnr = req.params.orgnr;
-
+  
     try {
-        const [brregResponse, dataNorgeResponse] = await Promise.all([
-            axios.get(`https://data.brreg.no/enhetsregisteret/api/enheter/${orgnr}`),
-            axios.get(`https://data.norge.no/organizations/${orgnr}`), 
-        ]);
+      const [brregResponse, dataNorgeResponse] = await Promise.all([
+        axios.get(`https://data.brreg.no/enhetsregisteret/api/enheter/${orgnr}`),
+        axios.get(`https://data.norge.no/organizations/${orgnr}`),
+      ]);
+  
+      console.log('Brreg response:', brregResponse.data);
+      console.log('Data Norge response:', dataNorgeResponse.data);
 
-        const companyData = {
-            ...brregResponse.data,
-            additionalData: dataNorgeResponse.data 
-        };
+      const companyData = {
+        organisasjonsnummer: brregResponse.data.organisasjonsnummer,
+        navn: brregResponse.data.navn,
+        organisasjonsform: brregResponse.data.organisasjonsform?.beskrivelse || '',
+        adresse: brregResponse.data.forretningsadresse?.adresse.join(', ') || '',
+        additionalData: {
+          hjemmeside: dataNorgeResponse.data.hjemmeside || '',
+          naeringskode1: brregResponse.data.naeringskode1?.beskrivelse || '',
+          antallAnsatte: brregResponse.data.antallAnsatte || '',
+          postnummer: brregResponse.data.forretningsadresse?.postnummer || '',
+          poststed: brregResponse.data.forretningsadresse?.poststed || '',
+        },
+      };
+  
+      console.log('Brreg response:', brregResponse.data);
+      console.log('Data Norge response:', dataNorgeResponse.data);
 
-        const existingCompanyIndex = companies.findIndex(company => company.orgnr === companyData.orgnr);
-        if (existingCompanyIndex >= 0) {
-            companies[existingCompanyIndex] = companyData;
-        } else {
-            companies.push(companyData);
-        }
-
-        res.json(companyData);
+      res.json(companyData);
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'An error occurred while fetching data.' });
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while fetching data.' });
     }
-});
+  });
+  
 
 app.post('/company', (req, res) => {
-    const companyData = req.body;
-    const existingCompanyIndex = companies.findIndex(company => company.orgnr === companyData.orgnr);
+  const companyData = req.body;
+  const existingCompanyIndex = companies.findIndex(
+    (company) => company.organisasjonsnummer === companyData.organisasjonsnummer
+  );
 
-    if (existingCompanyIndex >= 0) {
-        companies[existingCompanyIndex] = {...companies[existingCompanyIndex], additionalData: {...companies[existingCompanyIndex].additionalData, note: companyData.additionalData.note }};
-    } else {
-        companies.push(companyData);
-    }
+  if (existingCompanyIndex >= 0) {
+    companies[existingCompanyIndex] = {
+      ...companies[existingCompanyIndex],
+      additionalData: {
+        ...companies[existingCompanyIndex].additionalData,
+        note: companyData.additionalData.note,
+      },
+    };
+  } else {
+    companies.push(companyData);
+  }
 
-    res.status(200).json({ message: 'Company added/updated successfully.' });
+  res.status(200).json({ message: 'Company added/updated successfully.' });
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000');
 });
